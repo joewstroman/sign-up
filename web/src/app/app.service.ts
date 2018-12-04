eca58a8865cb2d9a138305d16ce165984fc9687b
Joseph Stroman <joewstroman@gmail.com>
2018-12-04 04:40:03 -0500

    Rudimentary authentication for web requests

--- web/src/app/app.service.ts
+++ web/src/app/app.service.ts
@@ -2,19 +2,42 @@ import { IAppState } from './store';
 
 const DBHOST = 'http://localhost:5000';
 
+const request = async (url: string, options: any = {}, headers: any = {}) => {
+    const opts = {
+        method: 'POST',
+        cache: 'no-cache',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            ...headers
+        },
+        ...options
+    };
+
+    return fetch(url, opts).catch((e) => alert(e));
+};
+
 export class SignUpService {
 
-    authenticateNewUser(userInfo: IAppState) {
-        fetch(`${DBHOST}/api/account`, {
-            method: 'POST',
-            cache: 'no-cache',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-            body: JSON.stringify({email: userInfo.email, password: userInfo.password})
-        }).then( async (resp: Response) => {
-            console.log(await resp.text());
-        }).catch( e => alert(e));
+    async authenticateNewUser(userInfo: IAppState) {
+        const resp = await request(`${DBHOST}/api/account`, { body: JSON.stringify({ email: userInfo.email, password: userInfo.password})});
+
+        if (resp && resp.ok) {
+            const responseBody = await resp.json();
+            this.addInfo(userInfo, responseBody.access_token);
+        }
+    }
+
+    async addInfo(userInfo: IAppState, accessToken: string) {
+        const info = {
+            name: userInfo.name,
+            lastName: userInfo.surname,
+            email: userInfo.email,
+            company: userInfo.company
+        };
+
+        const options = { body: JSON.stringify(info) };
+        const headers = { 'Authorization': `Bearer ${accessToken}` };
+        const resp = await request(`${DBHOST}/api/user`, options, headers);
     }
 }
